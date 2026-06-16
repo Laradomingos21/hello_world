e32e1e1139650f939307fff0467013f46e8d66a1
Laradomingos21 <lara.v.domingos@aluno.senai.br>
2026-06-16 10:40:44 -0300

    first commit

--- lib/Grupo 6/main_Exemplo2.dart
+++ lib/Grupo 6/main_Exemplo2.dart
@@ -3,47 +3,47 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
-  runApp(Idioma());
+  runApp(TemaApp());
 }
 
 // - - - - - - - Classe principal do aplicativo - - - - - - -
-class Idioma extends StatefulWidget {
-  const Idioma({super.key});
+class TemaApp extends StatefulWidget {
+  const TemaApp({super.key});
 
   @override
-  State<Idioma> createState() => _MyAppState();
+  State<TemaApp> createState() => _MyAppState();
 }
 
 // - - - - - - - Classe responsável pela lógica - - - - - - -
-class _MyAppState extends State<Idioma> {
+class _MyAppState extends State<TemaApp> {
   // Variável responsável por controlar o tema
   // true  -> tema escuro
   // false -> tema claro
   // O aplicativo começa no tema claro
-  bool valorIdioma = false;
+  bool isDarkTheme = false;
 
   @override
   void initState() {
     super.initState();
 
     // Quando o aplicativo inicia, ele chama a função carregarTema(). Essa função verifica se existe um tema salvo no SharedPreferences
-    carregarIdioma();
+    carregarTema();
   }
 
   // - - - - - - - Função para salvar o tema - - - - - - -
-  Future<void> salvarIdioma(bool valor) async {
+  Future<void> salvarTema(bool valor) async {
     final prefs = await SharedPreferences.getInstance();
 
-    await prefs.setBool('português', valor);
+    await prefs.setBool('temaEscuro', valor);
   }
 
   // - - - - - - - Função para carregar tema - - - - - - -
-  Future<void> carregarIdioma() async {
+  Future<void> carregarTema() async {
     final prefs = await SharedPreferences.getInstance();
-    bool? idiomaSalvo = prefs.getBool('português');
+    bool? temaSalvo = prefs.getBool('temaEscuro');
 
     setState(() {
-      valorIdioma = idiomaSalvo ?? false;
+      isDarkTheme = temaSalvo ?? false;
     });
   }
 
@@ -53,9 +53,17 @@ class _MyAppState extends State<Idioma> {
       // Remove a faixa DEBUG
       debugShowCheckedModeBanner: false,
 
+      // Tema claro padrão
+      theme: ThemeData.light(),
+
+      // Tema escuro
+      darkTheme: ThemeData.dark(),
+
+      themeMode: isDarkTheme ? ThemeMode.dark : ThemeMode.light,
+
       // - - - - - - - Tela Principal - - - - - - -
       home: Scaffold(
-        appBar: AppBar(title: const Text("Português e English")),
+        appBar: AppBar(title: const Text("Tema Claro e Escuro")),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
@@ -63,7 +71,7 @@ class _MyAppState extends State<Idioma> {
             children: [
               // Texto mostrando o tema atual
               Text(
-                valorIdioma ? "Hello" : "Olá",
+                isDarkTheme ? "Tema Escuro" : "Tema Claro",
 
                 style: const TextStyle(fontSize: 24),
               ),
@@ -73,53 +81,55 @@ class _MyAppState extends State<Idioma> {
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  // - - - - - - - Botão Portugues - - - - - - -
-                  ElevatedButton(
+                  // - - - - - - - Botão Sol - - - - - - -
+                  IconButton(
+                    icon: const Icon(Icons.wb_sunny, size: 40),
+
                     onPressed: () {
                       // Atualiza a tela
                       setState(() {
-                        // false = portugues
-                        valorIdioma = false;
+                        // false = tema claro
+                        isDarkTheme = false;
                       });
 
-                      // Salva o portugues
-                      salvarIdioma(false);
+                      // Salva o tema claro
+                      salvarTema(false);
                     },
-                    child: Text("Portugues"),
                   ),
 
                   const SizedBox(width: 10),
 
                   // SWITCH
                   Switch(
-                    value: valorIdioma,
+                    value: isDarkTheme,
 
                     onChanged: (valor) {
                       // Atualiza a tela
                       setState(() {
-                        valorIdioma = valor;
+                        isDarkTheme = valor;
                       });
 
                       // Salva no SharedPreferences
-                      salvarIdioma(valor);
+                      salvarTema(valor);
                     },
                   ),
 
                   const SizedBox(width: 10),
 
-                  // - - - - - - - Botão English - - - - - - -
-                  ElevatedButton(
+                  // - - - - - - - Botão Lua - - - - - - -
+                  IconButton(
+                    icon: const Icon(Icons.nightlight_round, size: 40),
+
                     onPressed: () {
                       // Atualiza a tela
                       setState(() {
-                        // true = ingles
-                        valorIdioma = true;
+                        // true = tema escuro
+                        isDarkTheme = true;
                       });
 
-                      // Salva o ingles
-                      salvarIdioma(true);
+                      // Salva o tema escuro
+                      salvarTema(true);
                     },
-                    child: Text("English"),
                   ),
                 ],
               ),
